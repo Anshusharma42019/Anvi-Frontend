e1c3dc76eadc07477d5165337306af86b4db974b
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2025-09-22 16:17:52 +0530

    sdd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  }
 })
